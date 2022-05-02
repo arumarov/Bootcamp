f55a18a1b3f5702b24b259425d521f1900651022
AntonUmarov <arumarov@yandex.ru>
2022-05-02 18:36:28 +0600

    Correct task1

---
+++ Programming/Seminar_one.cs
@@ -0,0 +1,17 @@
+// Написать программу, которая на вход получает строку,
+// а на выходе выдает строку, в которой удалены все символы
+
+// ваыикка89мамвкм9 >==> ваыиккамамвкм
+
+string ClearString(string s)
+{
+    string.Result = String.Empty;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] = '1') { }
+        else result = result + $"{s[i]}";
+    }
+    return result;
+}
+
+Console.WriteLine(ClearString("ваыикка89мамвкм9"));
