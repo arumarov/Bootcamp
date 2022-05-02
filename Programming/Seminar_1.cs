68d542a6459ecf703b2cfe2d48a698057bdfe2c3
AntonUmarov <arumarov@yandex.ru>
2022-05-02 18:42:44 +0600

    Correct task1

--- Programming/Seminar_1.cs
+++ Programming/Seminar_1.cs
@@ -1,14 +1,15 @@
-// Написать программу, которая на вход получает строку,
+﻿// Написать программу, которая на вход получает строку,
 // а на выходе выдает строку, в которой удалены все символы
 
 // ваыикка89мамвкм9 >==> ваыиккамамвкм
 
 string ClearString(string s)
 {
-    string.Result = String.Empty;
+    string result = String.Empty;
     for (int i = 0; i < s.Length; i++)
     {
-        if (s[i] = '1') { }
+        if (s[i] == '1'
+        ) { }
         else result = result + $"{s[i]}";
     }
     return result;
