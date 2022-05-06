e395918c6052ac23312d5c7d9fe76bed55a26a3d
arumarov <arumarov@yandex.ru>
2022-05-06 21:54:01 +0600

    Create nes

--- Programming/Seminar_1.cs
+++ Programming/Seminar_1.cs
@@ -8,11 +8,33 @@ string ClearString(string s)
     string result = String.Empty;
     for (int i = 0; i < s.Length; i++)
     {
-        if (s[i] == '1'
+        if (
+            s[i] == '1'
+            || s[i] == '2'
+            || s[i] == '8'
+            || s[i] == '9'
+            || s[i] == '0'            
         ) { }
         else result = result + $"{s[i]}";
     }
     return result;
 }
 
+void ClearStringTest01() // описываем тест
+{
+    bool res = "ваыиккамамвкм"
+    == ClearString("ваыикка89мамвкм9");
+    System.Console.WriteLine(res);
+}
+
+void ClearStringTest02() // описываем еще тест
+{
+    bool res = "ваыиккамамвкм"
+    == ClearString("ваыи2кка89мамвк32м9");
+    System.Console.WriteLine(res);
+}
+
 Console.WriteLine(ClearString("ваыикка89мамвкм9"));
+
+ClearStringTest01();
+ClearStringTest02();
